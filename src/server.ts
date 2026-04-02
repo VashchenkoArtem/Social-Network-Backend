42172035f53c77f36b800767cdd432439daeb501
Artem <artemvaschenko83@gmail.com>
2026-04-02 20:40:43 +0300

    Fixing bugs

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import type { Express } from "express"
 import express from "express"
+import cors from "cors"
+import { userRouter } from "./user"
 
 const HOST: string = "127.0.0.1"
 const PORT: number = 8000
 const app: Express = express()
 
+app.use(cors());
+app.use(express.json())
+app.use(userRouter)
 app.listen(PORT, HOST, () => {
     console.log(`Server is running at http://${HOST}:${PORT}`)
 })
