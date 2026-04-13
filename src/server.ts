95ac6e6a966b1838d3d04cc3e81953188dc66a84
Artem <artemvaschenko83@gmail.com>
2026-04-13 21:48:26 +0300

    Update d

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,14 @@ import express from "express";
 import type { Express } from "express";
 import cors from "cors";
 import { userRouter } from "./user/user.router";
+import { uploadDir } from "./config";
 
-const HOST = "192.168.0.107";
+const HOST = "192.168.0.104";
 const PORT = 8000;
 const app: Express = express();
 
 app.use(cors());
+app.use("/media/", express.static(uploadDir));
 app.use(express.json());
 app.use(userRouter);
 
--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
+import { uploadMiddleware } from "../middlewares/upload.middleware";
+
 
 export const userRouter = Router();
 
@@ -8,6 +10,6 @@ userRouter.post("/registration", userController.registration);
 userRouter.post("/send-code", userController.sendCode);
 userRouter.post('/login', userController.login);
 userRouter.get("/me", authMiddleware, userController.me);
-userRouter.patch("/update-user", authMiddleware, userController.updateUser);
+userRouter.patch("/update-user", uploadMiddleware.single('file'), authMiddleware, userController.updateUser);
 userRouter.post("/find-code", userController.getCode)
 userRouter.patch("/update-password", authMiddleware, userController.updatePassword)
