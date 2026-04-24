190af15cce8947e1d9d4a190156b8974ca655a4f
Artem <artemvaschenko83@gmail.com>
2026-04-24 20:30:59 +0300

    Fixing upload middleware

--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -12,18 +12,18 @@ export const uploadMiddleware = multer({ storage: memoryStorage() })
 export function procImgMiddleware(width: number, quality:number){
     return async (req: Request, res: Response, next: NextFunction) => {
         try {
-            if (!req.file){
+            if (!req.file && !req.body.alias && !req.body.nickname){
                 next(new ValidationError("file validation error"))
                 return
             }
             const fileName = `${Date.now()}.jpg`
             const filePathOriginal = join(originalDir, fileName);
             const filePathThumb = join(thumbDir, fileName);
-            
-            await sharp(req.file.buffer).flatten({ background: "#ffffff" }).toFile(filePathOriginal)
-            await sharp(req.file.buffer).resize({width}).flatten({ background: "#ffffff" }).jpeg({quality}).toFile(filePathThumb)
-            
-            req.file.filename = fileName
+            if (req.file){
+                await sharp(req.file.buffer).flatten({ background: "#ffffff" }).toFile(filePathOriginal)
+                await sharp(req.file.buffer).resize({width}).flatten({ background: "#ffffff" }).jpeg({quality}).toFile(filePathThumb)
+                req.file.filename = fileName
+            }
             next()
         } catch (error) {
             next(error)
